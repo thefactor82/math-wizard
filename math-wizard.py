370613a255bd380e8cc5a62ecefff896e7bece57
Mario Matteis <mmatteis@tierratelematics.com>
2026-08-26 17:48:25 +0200

    bump v1.3.33

--- math-wizard.py
+++ math-wizard.py
@@ -812,7 +812,7 @@ class Game :
         self .story_idx =0 
         self .num_story_levels =sum (1 for e in self .story_entries if e .get ("tipo")=="livello")
 
-        self .version ="1.3.32"
+        self .version ="1.3.33"
 
         self .profiles =[]
         self .current_profile =""
